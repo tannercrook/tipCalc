c9e4fc66b4d03c91cdadbb24a1d3fda84f27c73f
Tanner Crook <tacrook@DO-TaCrook-MBPro.local>
2014-12-16 06:50:42 -0700

    Update to Initial Commit
    Some minor changes to the initial commit.

--- tipCalc/ViewController.swift
+++ tipCalc/ViewController.swift
@@ -9,7 +9,35 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    // UI ELEMENTS
+    @IBOutlet weak var billField: UITextField!
+    
+    @IBOutlet weak var tenPercentButton: UIButton!
+    @IBOutlet weak var fifteenPercentButton: UIButton!
+    @IBOutlet weak var twentyPercentButton: UIButton!
+    
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    
+    
+    
+    
+    // BUTTON ACTION FUNCTIONS
+    @IBAction func tenPercentPressed(sender: AnyObject) {
+        
+    }
+    
+    @IBAction func fifteenPercentPressed(sender: AnyObject) {
+        
+    }
+    
+    @IBAction func twentyPercentPressed(sender: AnyObject) {
+        
+    }
+    
 
+    // OS FUNCTIONS
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
